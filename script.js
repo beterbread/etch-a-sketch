54124f9d57aedeea323f54802d92e1f644c2d079
Peter Kim <petertimmmybob113@gmail.com>
2023-05-20 12:39:32 -0500

    working grid

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('#container');
+for (let i = 0; i < 16; i++) {
+  const row = document.createElement('div');
+  row.style.display = 'flex';
+  for (let j = 0; j < 16; j++) {
+    const div = document.createElement('div');
+    let size = 960 / 16;
+    div.style.width = size + 'px';
+    div.style.height = size + 'px';
+    row.appendChild(div);
+  }
+  container.appendChild(row);
+}
+
+const boxes = document.querySelectorAll('div');
+boxes.forEach((box) => {
+  box.addEventListener('mouseover', (e) => {
+    e.stopPropagation();
+    box.style.backgroundColor = 'black'; 
+  });
+});
